ac21a46798168e4969dddbf35a3e6b4feebd6979
vladlen <vladl.rogov@yandex.ru>
2020-04-17 18:33:39 +0300

    Первый запуск

---
+++ Task2/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+
+                try
+                {
+                    Tasks tasks = new Tasks();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Введите пароль для входа в систему");
+                    string password = Console.ReadLine();
+                    if (password != "007700")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Пароль не верный вам отказано в доступе!");
+                        break;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Наберите 1 если хотите выполнить задачу TASK_1() ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("Описание программы номер 1 программа проверяет делится ли введенное" +
+                                      "пользователем число на 3 и на 7");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nНаберите 2 если хотите выполнить задачу task_2() ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("Описание программы номер 2 программа определяет какое " +
+                                      "из чисел больше или они равны!");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nНаберите 3 если хотите выполнить задачу task_3() ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("Описание программы номер 3. Программа отображает" +
+                                      "сумму введенных пользователем чисел");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nНаберите 4 если хотите выполнить задачу task_4() ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("Описание программы номер 4. Программа определяет " +
+                                      "по введенному пользователем числу день недели. ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nНаберите 5 если хотите выполнить задачу task_5() ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("Описание программы номер 5. Программа вычисляет сумму" +
+                                      " нечетных чисел");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nНабирите 6 если хотите выполнить задачу task_6() ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("Описание программы номер 6. Программа определяет числа " +
+                                      "Фибоначчи");
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+
+
+                    string choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                            tasks.TASK_1();
+                            break;
+                        case "2":
+                            tasks.task_2();
+                            break;
+                        case "3":
+                            tasks.task_3();
+                            break;
+                        case "4":
+                            tasks.task_4();
+                            break;
+                        case "5":
+                            tasks.task_5();
+                            break;
+                        case "6":
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Program is developing");
+                            break;
+                       default:
+                           Console.ForegroundColor = ConsoleColor.Red;
+                           Console.WriteLine("Программа не обработала введеное значение");
+                            break;
+
+
+                    }
+                    // Смотри сюда.
+                    Console.WriteLine("Мы попали сюда");
+
+
+
+
+                }
+
+
+
+
+                
+
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Вы болван вы ввели не число");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Хотите продолжить?");
+
+                    Console.WriteLine("Для повтора наберите Да, для выхода Нет");
+                    string rep = Console.ReadLine();
+                    if (rep == "Да")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Система перезагружается....");
+                        Main(args);
+                    }
+                    else
+                    {
+                        if (rep == "Нет")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Ну как хотите!");
+                        }
+                    }
+
+
+                }
+
+                Console.ReadLine();
+
+            }
+            while (true);
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task2/Tasks.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class Tasks
+    {
+        private string vlad = "vlad";
+        public void task_2()
+        {
+            // Напишите программу  в которой пользовалель вводит последовательно 2 целых числа
+            // Программа определяет какое из чисел больше или они равны
+            
+                Console.WriteLine("Введите первое целое число");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе целое число");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+                if (number > number2)
+                {
+                    Console.WriteLine($"Число {number} больше");
+                }
+                else
+                {
+                    if (number < number2)
+                    {
+                        Console.WriteLine($"Число {number2} больше");
+                    }
+                    else
+                    {
+                        if (number == number2)
+                        {
+                            Console.WriteLine($"Числа {number} и {number2} равны");
+                            
+                        }
+                    }
+                   
+                }
+                
+            
+           
+
+        }
+
+
+        public void task_3()
+        {
+            // Напишите программу в которой пользователь вводит два целых числа.
+            // а программа отображает сумму этих чисел.
+            // это продолжается до тех пор пока пользователь не введет нулевое значение.
+
+            int summa2 = 0; // холодильник
+
+            do
+            {
+                Console.WriteLine("Введите число для суммы ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число для суммы");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+                 
+                summa2 = summa2 + number+number2;
+               
+                if (number == 0 || number2 == 0) break;
+                
+                
+                Console.WriteLine($"Сумма чисел равна {summa2}");
+
+
+
+
+            }
+            while (true);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Вы ввели ноль программа закончилась");
+            
+
+
+
+
+
+        }
+
+       public void TASK_1()
+        {
+            // Напишите программу в которой пользовтель вводит число а программа
+            // проверяет делится ли это число на 3 и на 7
+            
+                Console.WriteLine("Введите число");
+                double number = Convert.ToDouble(Console.ReadLine());
+                if (number % 3 == 0 && number % 7 == 0)
+                {
+                    Console.WriteLine("Число делится");
+                }
+                else
+                {
+                    Console.WriteLine("Число не делится");
+                    
+                }
+                Console.ReadLine();
+          
+            
+           
+           
+
+
+        }
+
+        public void task_4()
+        {
+            // Напишите программу в которой пользователь вводит целое число в диапозоне от 1 до 7
+            // а программа определяет по этому числу день недели!
+
+            Console.WriteLine("Введите целое число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("Понедельник");
+                       
+                        break;
+                    case 2:
+                        Console.WriteLine("Вторник");
+                        
+                        break;
+                    case 3:
+                        Console.WriteLine("Среда");
+                       
+                        break;
+                    case 4:
+                        Console.WriteLine("Четверг");
+                       
+                        break;
+                    case 5:
+                        Console.WriteLine("Пятница");
+                       
+                        break;
+                    case 6:
+                        Console.WriteLine("Суббота");
+                        
+                        break;
+                    case 7:
+                        Console.WriteLine("Sunday");
+                        
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Вы вышли за допустимый диапазон");
+                        Console.WriteLine("Система перезагружается....");
+                        break;
+
+                }
+                if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5 || number == 6 || number == 7)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Программа закончена");
+                    Console.WriteLine("Система перезагружается....");
+                    break;
+                }
+            }
+            while (true);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Система перезагрузилась");
+            Console.WriteLine("Хотите продолжить");
+            Console.WriteLine("Для продолжение нажмите Да или Нет");
+            string rep = Console.ReadLine();
+            if (rep == "Да")
+            {
+                task_4();
+            }
+            else
+            {
+                if (rep == "Нет")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Как скажите");
+                    Console.WriteLine("Выход из системы");
+                }
+            }
+            Console.ReadLine();
+
+
+
+        }
+
+        public void task_5()
+        {
+            // Напишите программу в которой вычисляется сумма нечетных чисел.
+            do
+            {
+                Console.WriteLine("Введите число");
+                int number = Convert.ToInt32(Console.ReadLine());
+                int summa = 0;
+                for (int i = 1; i <= number; i++)
+                {
+                    if ((i % 2) == 1)
+                    {
+                        summa = summa + i;
+                    }
+                }
+                Console.WriteLine(summa);
+                break;
+
+            }
+            while (true);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Программа закончила свою работу");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Хотите продолжить?");
+
+            Console.WriteLine("Для повтора наберите Да, для выхода Нет");
+            string rep = Console.ReadLine();
+            if (rep == "Да")
+            {
+                task_5();
+                
+
+            }
+            else
+            {
+                if (rep == "Нет")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ну как хотите!");
+                    Console.WriteLine("Система перезапускается....");
+                    
+                }
+                else
+                {
+                    if (rep == "")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Введенно некоректное значение");
+                    }
+                }
+                
+            }
+
+            Console.ReadLine();
+
+        }
+        public void task_6()
+        {
+            //  Напишите программу в которой выводится последовательность чисел Фибоначчи
+            Console.WriteLine("Введите  элемент");
+            int alement = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второй элемен");
+            int summa = 0;
+            for(int i = 1; i <= alement; i++)
+            {
+
+            }
+
+
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
